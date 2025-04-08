2a3d4ba2010059c1cb2db2d929d24bf57c499394
Kyle Mordan <kyle.mordan@gmail.com>
2025-04-07 22:49:51 -0600

    Add Rubocop (#2)
    * add rubocop gems
    
    * add rubocop config
    
    * correct rubocop config and apply lint changes
    
    * cleanup config

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   describe 'validations' do
     it 'requires a name' do
-      event = Event.new(starts_at: Time.current, ends_at: Time.current + 1.hour)
+      event = described_class.new(starts_at: Time.current, ends_at: Time.current + 1.hour)
       expect(event).not_to be_valid
       expect(event.errors[:name]).to include("can't be blank")
     end
 
     it 'requires a start time' do
-      event = Event.new(name: "Test Event", ends_at: Time.current + 1.hour)
+      event = described_class.new(name: 'Test Event', ends_at: Time.current + 1.hour)
       expect(event).not_to be_valid
       expect(event.errors[:starts_at]).to include("can't be blank")
     end
 
     it 'requires an end time' do
-      event = Event.new(name: "Test Event", starts_at: Time.current)
+      event = described_class.new(name: 'Test Event', starts_at: Time.current)
       expect(event).not_to be_valid
       expect(event.errors[:ends_at]).to include("can't be blank")
     end
